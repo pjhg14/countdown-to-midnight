f8dbe96e91fd950e0d2fbdc48e3fded490414e93
pj <pjhg14@gmail.com>
2021-04-25 12:54:15 -0400

    commit tree faf1690f2680a40912d27080e9fff132e757373b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    count = number
+
+    count.times do
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+    count = number
+
+    number.times do
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        sleep(1)
+    end
+
+    "HAPPY NEW YEAR!"
 end
